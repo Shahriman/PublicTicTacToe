4870ef37140dbf6017eb064aa560568e3ee605ba
student <student@pDTLAB-CC-00.student.newinti.edu.my>
2022-05-09 13:26:04 +0800

    Next week no class

--- TicTacToe/tic.cpp
+++ TicTacToe/tic.cpp
@@ -1,6 +1,6 @@
 //Edited by Shahriman on 9 May
 //Today is Monday
-//I am hungry
+//I am hungry, I want to eat fried rice.
 //Tic Tac Toe Game in C++
 //Importing the inbuild libraries in CPP
 #include <iostream>
